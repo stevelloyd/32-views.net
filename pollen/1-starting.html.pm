8c9c3e5b132bccb66ddd2f0d8932373b4b6a6760
Steve Lloyd <stevel@kinoteki.com>
2018-12-29 12:23:28 +0000

    edits to index and beginnings of starting

--- pollen/1-starting.html.pm
+++ pollen/1-starting.html.pm
@@ -12,7 +12,7 @@
 
 \h3{Set your Pi up}
 
-You'll need to get a Pi for these projects---there are lots of suppliers (see \em{notes}). You can buy the parts individually, or get a 'starter kit' which has all the modules to get going. Then you'll need to plug together the bits. There are several models of Pi, and the best if you're going to get the best out of these projects to start with, is the most recent Raspberry Pi 3 Model B+. Later in the server
+You'll need to get a Pi for these projects---there are lots of suppliers (see \em{notes}). You can buy the parts individually, or get a 'starter kit' which has all the modules to get going. Then you'll need to plug together the bits. There are several models of Pi, and the best if you're going to get the best out of these projects to start with, is the most recent Raspberry Pi 3 Model B+. In later projects, we'll use the Pi 3 Model B+'s little cub; the Zero W which is great as a little project machine, but for now, try and get the Big Bear of Pi.
 
 The Raspberry Pi Foundation website has excellent resources on setup of a Pi---well it has excellent resources on so many topics that it's best just to take a look. But what we're looking for is the intial setup covered by \a[#:href "https://projects.raspberrypi.org/en/projects/raspberry-pi-setting-up"]{this}. 
 
--- pollen/index.html.pm
+++ pollen/index.html.pm
@@ -5,7 +5,7 @@
 
 \img[#:src "images/32views-lowres.png" #:alt "A dragon curve drawn in Racket"]{}
 
-\p{\em{First instalment coming this Autumn}}
+\p{\em{First instalment coming shortly}}
 
 So, you’re looking at something called \em{32 views of computing—with Racket and Raspberry Pi}. 
 
@@ -16,7 +16,7 @@ So, you’re looking at something called \em{32 views of computing—with Racket
 
 It's a series of projects, in which we'll learn together about how to use a programming language called \em{Racket}, running on the small, affordable Raspberry Pi computer \span[#:class "sidebar"]{Raspberry Pi is a trademark of the Raspberry Pi Foundation}. The idea of the projects will be to interact with the real world (flashing lights, sensing environmental parameters such as temperature) and presenting the information sensed. Along the way we'll pick up some general computing skills.
 
-The projects will be released in installments (the first installment is parts 1 and 2).
+The projects will be released in installments.
 
 \h2{Why the name '32 views…'?}
 
@@ -43,7 +43,7 @@ which is \em{The Great Wave}.
 I've co-opted Hokusai-sensei's publishing idea–hence \em{32 views of computing—with Racket and Raspberry Pi}. I
 like the multiple \em{views} notion, because it provides both me, and 
 you the reader with small, hopefully approachable chunks to read/make. And \em{'32'}
-because computers like powers of  \a[#:href "https://en.wikipedia.org/wiki/Power_of_two"]{powers of
+because computers like powers of \a[#:href "https://en.wikipedia.org/wiki/Power_of_two"]{powers of
 two}!
 
 \h2{Why Racket?}
@@ -79,22 +79,22 @@ I guess the first machines which made an impression on me were the trains  passi
 
 Later, my dad was keen on the \a[#:href "https://en.wikipedia.org/wiki/Timeline_of_the_BBC#1960s"]{experimental stereo transmissions} broadcast by the BBC and I watched him assemble the kit to listen in--including homemade speaker cabinets.
 
-My mum was a committed Primary (Kindergarden) teacher, and a life-long maker. Observing her creating teaching materials to engage her students is a memory that stays with me, as is the orange velvet jacket I cajoled her to make for me from a discarded curtain as a teenager (in my defence it was the 1970s). 
-
-And one of my most prized books is her copy of \a[#:href "https://en.wikipedia.org/wiki/Mindstorms_(book)"]{Mindstorms}.
+My mum was a committed Primary (Kindergarden) teacher, and a life-long maker. Observing her creating teaching materials to engage her students is a memory that stays with me, as is the orange velvet jacket I cajoled her to make for me from a discarded curtain as a teenager (in my defence it was the 1970s). One of my most prized books is her copy of \a[#:href "https://en.wikipedia.org/wiki/Mindstorms_(book)"]{Mindstorms}.
 
 Amateur (ham) radio started me off in electronics making, and a University project to build a microcomputer got me interested in computing machinery, which developed into 'making' jobs in Healthcare, Environmental Research, Computer Architecture and Publishing.
 
-Machines have always fired my imagination---from Robbie the Robot in \a[#:href "https://en.wikipedia.org/wiki/Fireball_XL5"]{Fireball XL5} via Bomb no. 20 in \a[#:href "https://en.wikipedia.org/wiki/Dark_Star_%28film%29"]{Dark Star} to the gentle robots of \a[#:href "https://en.wikipedia.org/wiki/Castle_in_the_Sky"]{Laputa}.
+Machines have always also fired my imagination---from Robbie the Robot in \a[#:href "https://en.wikipedia.org/wiki/Fireball_XL5"]{Fireball XL5} via Bomb no. 20 in \a[#:href "https://en.wikipedia.org/wiki/Dark_Star_%28film%29"]{Dark Star} to the gentle robots of \a[#:href "https://en.wikipedia.org/wiki/Castle_in_the_Sky"]{Laputa}.
 
-When I had the later realization that with computers, it was possible to build imagined machines in software, it was a revelation. And also the notion that computing applications themselves are a tower of machines; the underlying microcode pretending to be the CPU which executes the code, which is pretending to be a game, a car engine controller…
+When I had the later realization that with computers, it was possible to build imagined machines in software, it was a revelation. And also the notion that computing applications themselves are a tower of machines; the underlying microcode 
+\span[#:class "sidebar"]{Microcode is the basic code often used to define the features of a processor}
+pretending to be the CPU which executes the code, which is pretending to be a game, a car engine controller…
 
 So, just to say there are going to be machines; built in both software and hardware, possibly even fabric.
 
 
 \h2{Do I have to pay for this?}
 
-Some project resources may require payment, and you'll need to buy or borrow some components to build with your Pi. You're most welcome to contribute to fund the installments as they appear. But this online text will be available for free.
+Some project resources will require payment, and you'll need to buy or borrow some components to build with your Pi. But this online text will be available for free.
 
 You'll see at the bottom of each page that the projects are released under a Creative Commons Atribution-ShareAlike licence, which gives you a lot of freedom to adapt and share the content.
 
